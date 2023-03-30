81fddca444cb4b4898e2bfa7f8ffa59967d02658
AzureAD\JemaicaMaeMon <jmon@dunbraesubic.ph>
2023-03-30 20:08:03 +0800

    add projects hover

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,13 +4,48 @@ const Projects = () => {
             <div className="heading">Projects</div>
             <div className="card_container">
                 <div className="card">
-                    <header className="card_header">
-                        <h3>Project Name</h3>
-                    </header>
-                    <div className="card_body">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quisquam laborum dolor minima soluta delectus perspiciatis dolore perferendis ea animi?</p>
+                    
+                    <img src="../assets/images/img-9.jpg" alt="" />
+                    <div className="card_contents">
+
+                        <header className="card_header">
+                            Project Name
+                        </header>
+                        <div className="card_body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quisquam laborum dolor minima soluta delectus perspiciatis dolore perferendis ea animi?</p>
+                            <span class="badge badge--primary badge--small">ReactJs</span>
+                            <span class="badge badge--primary badge--small">MongoDB</span>
+                        </div>
                     </div>
                 </div>
+                {/* <div className="card">
+
+                    <img src="../assets/images/img-2.jpg" alt="" />
+                    <div className="text">
+
+                        <header className="card_header">
+                            <h3>Project Name</h3>
+                        </header>
+                        <div className="card_body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quisquam laborum dolor minima soluta delectus perspiciatis dolore perferendis ea animi?</p>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+                <div className="card">
+
+                    <img src="../assets/images/img-3.jpg" alt="" />
+                    <div className="text">
+
+                        <header className="card_header">
+                            <h3>Project Name</h3>
+                        </header>
+                        <div className="card_body">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quisquam laborum dolor minima soluta delectus perspiciatis dolore perferendis ea animi?</p>
+                            <span class="badge badge--primary badge--small">10% Off</span>
+                        </div>
+                    </div>
+                </div> */}
             </div>
 
         </>
