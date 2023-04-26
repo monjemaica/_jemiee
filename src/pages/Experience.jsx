c33b26c9b571837251dde5923c49e513efdbcd56
AzureAD\JemaicaMaeMon <jmon@dunbraesubic.ph>
2023-04-26 13:58:55 +0800

    tab size

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -7,14 +7,15 @@ const Experience = ({ exp, onSelected, isActive }) => {
         <>
             <div className="heading">Experience</div>
             <div className="box-container">
-
-                <ul className="list-group">
-                    {getCompany().map(e =>
-                        <li key={e.id} onClick={() => onSelected(e.id)} className={isActive === e.id ? "list--item active" : "list--item"}>
-                            {e.company}
-                        </li>
-                    )}
-                </ul>
+                <div>
+                    <ul className="list-group">
+                        {getCompany().map(e =>
+                            <li key={e.id} onClick={() => onSelected(e.id)} className={isActive === e.id ? "list--item active" : "list--item"}>
+                                {e.company}
+                            </li>
+                        )}
+                    </ul>
+                </div>
                 <div className="content">
                     <Content exp={exp} />
                 </div>
@@ -27,30 +28,8 @@ const Experience = ({ exp, onSelected, isActive }) => {
                     </div>
 
                 </div>
-                
-            </div>
-            {/* <div className="exp--container">
-                <div className="box--container">
 
-                    <ul className="list-group">
-                        {getCompany().map(e =>
-                            <li key={e.id} onClick={() => onSelected(e.id)} className={isActive === e.id ? "list--item active" : "list--item"}>
-                                {e.company}
-                            </li>
-                        )}
-                    </ul>
-                    <div className="content">
-                        <Content exp={exp} />
-                    </div>
-                </div>
-                <div className="square--container">
-                    <div className="square--img">
-                        {exp.map(res =>
-                            <img src={res.img} alt="" />
-                        )}
-                    </div>
-                </div>
-            </div> */}
+            </div>
 
 
         </>
--- src/services/companyData.js
+++ src/services/companyData.js
@@ -1,7 +1,7 @@
 export const company = [
     {
       id: "9d384aae-4f79-4f82-8034-0e77f84a115e",
-      company: "Dunbrae Subic Inc."
+      company: "Dunbrae Subic"
     },
     {
       id: "f4cce117-5f69-46f5-9037-ab541edd732e",
@@ -9,7 +9,7 @@ export const company = [
     },
     {
       id: "09ff2d76-e177-4227-b9b5-47f1cd826d0e",
-      company: "Easypos Solution Inc."
+      company: "Easypos Solution"
     }
   ];
   
