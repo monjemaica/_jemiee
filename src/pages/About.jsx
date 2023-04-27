781b5a6c460d80d5b42cc0d83287a6fb52143233
monjemaica <201810138@gordoncollege.edu.ph>
2023-04-28 00:02:26 +0700

    add about--img

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,32 +2,42 @@
 const About = () => {
     return (
         <>
-            <div className="separator">
-                <span className="heading--no">01. &nbsp;</span>
-                <span className="heading">
-                    About
-                </span>
-            </div>
-            <p className="about--paragraph">
-                Mabuhay! I'm Jem, a passionate and dedicated Full-Stack developer that build things in internet
-                based in Olongapo City, Philippines. I have developed many types of API development, Websites and
-                Mobile Apps using Angular/ Ionic and Laravel Framework.
-            </p>
-            <p className="about--paragraph">
-                Interested in the entire developement of backend and working on ambitious projects with positive people.
-            </p>
+            <div className="box-container ">
+                <div className="square--container img--sm">
+                    <div className="square--img">
+                        <img src="../assets/images/img-dp.jpg" alt="" />
+                    </div>
+                </div>
+                <div className="about--content">
 
-            <p className="about--paragraph">
-                Here are a few Programming Language/ Frameworks I've been working with recently:
-            </p>
-            <ul className="list list--tick list--col2">
-                <li className="list__item">JavaScript</li>
-                <li className="list__item">ReactJs</li>
-                <li className="list__item">Node.js</li>
-                <li className="list__item">Laravel</li>
-                <li className="list__item">Angular</li>
-                <li className="list__item">Ionic</li>
-            </ul>
+                    <div className="separator">
+                        <span className="heading--no">01. &nbsp;</span>
+                        <span className="heading">
+                            About
+                        </span>
+                    </div>
+                    <p className="about--paragraph">
+                        Mabuhay! I'm Jem, a passionate and dedicated Full-Stack developer that build things in internet
+                        based in Olongapo City, Philippines. I have developed many types of API development, Websites and
+                        Mobile Apps using Angular/ Ionic and Laravel Framework.
+                    </p>
+                    <p className="about--paragraph">
+                        Interested in the entire developement of backend and working on ambitious projects with positive people.
+                    </p>
+
+                    <p className="about--paragraph">
+                        Here are a few Programming Language/ Frameworks I've been working with recently:
+                    </p>
+                    <ul className="list list--tick list--col2">
+                        <li className="list__item">JavaScript</li>
+                        <li className="list__item">ReactJs</li>
+                        <li className="list__item">Node.js</li>
+                        <li className="list__item">Laravel</li>
+                        <li className="list__item">Angular</li>
+                        <li className="list__item">Ionic</li>
+                    </ul>
+                </div>
+            </div>
 
         </>
     );
