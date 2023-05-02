6d5c8c6ba125303c1638613f21172d50201cbf13
AzureAD\JemaicaMaeMon <jmon@dunbraesubic.ph>
2023-05-02 16:02:49 +0800

    add header

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -7,7 +7,8 @@ const HeroSection = () => {
     return (
         <>
             <div className="hero-container">
-                <video src="/assets/videos/video-3.mp4" autoPlay loop muted />
+                {/* <video src="/assets/videos/pixelart.gif" autoPlay loop muted /> */}
+                <img src="/assets/videos/goldfish.gif" alt="" />
                 <span className='hero--heading small'>Hi, I'm</span>
                 <span className='hero--heading big'>Jemaica Mon</span>
                 <span className='hero--heading role'>Full-Stack Developer / Software Engineering</span>
@@ -17,6 +18,7 @@ const HeroSection = () => {
                     {/* <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>GET STARTED</Button>
                     <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>Watch Trailer <i className={`far fa-play-circle ${styles["fa-play-circle"]}`} /> </Button> */}
                 </div>
+                {/* <div className='hero--heading ref'><a href='https://www.artstation.com/artwork/GawB1z'>https://www.artstation.com/artwork/GawB1z</a></div> */}
             </div>
         </>
     );
