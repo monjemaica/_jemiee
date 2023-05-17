757fac8ec589269deda217e19870bec561ba6f80
AzureAD\JemaicaMaeMon <jmon@dunbraesubic.ph>
2023-05-17 08:34:16 +0800

    update daTA

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,14 +5,19 @@ import '../styles.css';
 const Content = ({ exp }) => {
     return (
         <>
-    
-            {exp.map((res, i )=> 
+
+            {exp.map((res, i) =>
                 <div key={i}>
-                    <div className="content--header">{res.company}</div>
+                    <div className="card_header">{res.role}</div>
+                    <div className="content--header hover-underline-animation">
+                        <a href={res.link}>
+                            {res.company}
+                        </a>
+                    </div>
                     <div className='content--date'>{res.duration}</div>
 
                     <ul className="content--desc list list--tick">
-                        {res.description.map((desc, i )=> 
+                        {res.description.map((desc, i) =>
                             <li key={i} className="list__item">{desc}</li>
                         )}
                     </ul>
--- src/services/experienceData.js
+++ src/services/experienceData.js
@@ -2,14 +2,15 @@ export const exps = [
   {
     id: "9d384aae-4f79-4f82-8034-0e77f84a115e",
     company: "Dunbrae Subic Inc.",
+    role: "Full-Stack Developer",
     description:[
-      "Developing API development and automations using PHP and MySQL.",
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore reiciendis corporis accusantium! Excepturi, sed ullam perferendis dicta nam voluptate assumenda hic necessitatibus delectus repellat, exercitationem ipsam praesentium sapiente quis dolor atque animi vel earum adipisci iusto corrupti dolore aperiam tempora? Obcaecati, reiciendis. Maiores repudiandae eos impedit quo animi deserunt quia?",
-      "Developing API development and automations using PHP and MySQL.",
-      "Developing API development and automations using PHP and MySQL.",
-    "Developing API development and automations using PHP and MySQL."
+      "Build API development and automations using Laravel, PHP and MySQL located at Subic Bay Freeport Zone",
+      "Assist the business in terms of support, enhancement and development of applications and databases, interfaces and data.",
+      "Work with all the relevant departments such as Call Centers, Invoicing and Spare Parts to advance research and development to leverage emerging technologies and services.",
+      "Provide technical assistance and backup to the support teams to resolve issues impacting on our systems and data availability"
     ],
-    duration: "August - Present",
+    duration: "August 2022 - Present",
+    link: "https://dunbraesubic.ph/",
     img: [
       "../assets/images/dunbrae1.jpg",
       "../assets/images/dunbrae2.jpg",
@@ -19,23 +20,35 @@ export const exps = [
   {
     id: "f4cce117-5f69-46f5-9037-ab541edd732e",
     company: "Gordon College",
-    description: ["Part-time instructor teaching Computer Programming."],
-    duration: "August - Present",
+    role: "Computer Instructor",
+    description: [
+      "Part-time instructor teaching Computer Programming to Computer Science and ACT 1st year college students.",
+      "Responsible for providing instruction on a wide range of computer science courses, including an introductory survey course, Programming with C Language and Java, and/or college-level computer science courses.",
+      "Teach how to write computer programs, algorithms and programming languages."
+    ],
+    duration: "August 2022 - August 2023",
+    link: "https://gordoncollegeccs.edu.ph/ccs/ccsweb/#/",
     img: [
-      "../assets/images/img-10.jpg",
-      "../assets/images/img-10.jpg",
-      "../assets/images/img-10.jpg",
+      "../assets/images/gc1.jpg",
+      "../assets/images/gc3.jpg",
+      "../assets/images/gc2.jpg",
     ]
   },
   {
     id: "09ff2d76-e177-4227-b9b5-47f1cd826d0e",
     company: "Easypos Solution Inc.",
-    description: ["Completing 400 hours OJT as an Android Mobile Dev."],
+    role: "Android Developer Intern",
+    description: [
+      "Completing 400 hours OJT as an Android Mobile Developer.",
+      "Designing and developing advanced applications for the Android platform using Kotlin and Android Studio",
+      "Bug fixing and improving application performance"
+    ],
     duration: "March - June 2022",
+    link: "https://easypos.com.ph/",
     img: [
-      "../assets/images/img-10.jpg",
-      "../assets/images/img-10.jpg",
-      "../assets/images/img-10.jpg",
+      "../assets/images/easypos1.jpg",
+      "../assets/images/easypos2.jpg",
+      "../assets/images/easypos3.jpg",
     ]
   },
 ];
